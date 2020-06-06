23edc2b840ca1fa4d90687f9bb10d0a9dab3c8e3
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-06 01:07:30 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,15 +1,15 @@
 class Waiter
- 
+
     attr_accessor :name, :yrs_experience
-   
+
     @@all = []
-   
+
     def initialize(name, yrs_experience)
       @name = name
       @yrs_experience = yrs_experience
       @@all << self
     end
-   
+
     def self.all
       @@all
     end
@@ -28,8 +28,8 @@ class Waiter
         best_tipped_meal = meals.max do |meal_a, meal_b|
           meal_a.tip <=> meal_b.tip
         end
-       
+
         best_tipped_meal.customer
     end
-   
+
   end
